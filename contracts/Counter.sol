be99bf290630ab83596039a66970000b7b6104a9
unknown <moyanopaulas@gmail.com>
2022-04-12 15:55:25 -0300

    Dapp Smart Contract

---
+++ contracts/Counter.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.12;
+
+contract Counter {
+    uint256 public counter = 23;
+    function increment() public returns (uint256) {
+        counter = counter + 1;
+        return counter;
+    }
+    function decrement() public returns (uint256) {
+        counter = counter - 1;
+        return counter;
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+const Counter = artifacts.require("../contracts/Counter.sol");
+
+module.exports = function (deployer) {
+  
+  deployer.deploy(Counter);
+};
